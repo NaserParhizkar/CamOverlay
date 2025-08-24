3c80e5ea56a712cd44784adb86eaa7d633894b1b
Naser <Naser@DESKTOP-K10JTNL>
2025-08-24 23:00:33 +0330

    Changes for rate for image and green square around face

--- CamOverlay/MainWindow.axaml.cs
+++ CamOverlay/MainWindow.axaml.cs
@@ -10,5 +10,15 @@ namespace CamOverlay
             DataContext = new MainWindowViewModel();
 
         }
+
+        //// Ensure initial measurement on window open
+        //private void Window_Opened(object sender, System.EventArgs e)
+        //{
+        //    if (BorderMainImage.Bounds.Width > 0 && BorderMainImage.Bounds.Height > 0)
+        //    {
+        //        double overlaySize = Math.Min(BorderMainImage.Bounds.Width, BorderMainImage.Bounds.Height) * 0.1;
+        //        _viewModel.OverlaySize = overlaySize;
+        //    }
+        //}
     }
 }
--- CamOverlay/MainWindowViewModel.cs
+++ CamOverlay/MainWindowViewModel.cs
@@ -13,6 +13,77 @@ namespace CamOverlay
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
+        //private double _imageWidth;
+        //private double _imageHeight;
+        //private double _overlaySize;
+        //private double _scale = 0.6; // 60%, change to 0.7 for 70%
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //public double ImageWidth
+        //{
+        //    get => _imageWidth;
+        //    set
+        //    {
+        //        if (_imageWidth != value)
+        //        {
+        //            _imageWidth = value;
+        //            OnPropertyChanged();
+        //            UpdateOverlaySize();
+        //        }
+        //    }
+        //}
+
+        //public double ImageHeight
+        //{
+        //    get => _imageHeight;
+        //    set
+        //    {
+        //        if (_imageHeight != value)
+        //        {
+        //            _imageHeight = value;
+        //            OnPropertyChanged();
+        //            UpdateOverlaySize();
+        //        }
+        //    }
+        //}
+
+        //public double OverlaySize
+        //{
+        //    get => _overlaySize;
+        //    set
+        //    {
+        //        if (_overlaySize != value)
+        //        {
+        //            _overlaySize = value;
+        //            OnPropertyChanged();
+        //        }
+        //    }
+        //}
+
+        //public double Scale
+        //{
+        //    get => _scale;
+        //    set
+        //    {
+        //        if (_scale != value)
+        //        {
+        //            _scale = value;
+        //            OnPropertyChanged();
+        //            UpdateOverlaySize();
+        //        }
+        //    }
+        //}
+
+        //private void UpdateOverlaySize()
+        //{
+        //    var minDim = _imageWidth < _imageHeight ? _imageWidth : _imageHeight;
+        //    OverlaySize = minDim * _scale;
+        //}
+
+        //private void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
         private Bitmap? _currentFrame;
         private IBrush _guideBoxColor = Brushes.Lime;
         private string _statusText = "Starting...";
